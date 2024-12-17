0595d4a2162bd5615c13c9c443dffda93589b333
thesalmonification <33437091+thesalmonification@users.noreply.github.com>
2024-12-16 20:37:20 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,15 @@
 from setuptools import setup, find_packages
 
+with open('README.md', "r") as f:
+    description = f.read()
+
+
 setup(
     name='pyparous',
     version='0.1',
     packages=find_packages(),
     install_requires=[
-    ]
+    ],
+    long_description=description,
+    long_description_content_type="text/markdown",
 )
